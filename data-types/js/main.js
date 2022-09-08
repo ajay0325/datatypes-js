67b10637cd3a7bf1bb1f8b8f78ae01dd3684e3cd
ajay0325 <m.ajay030398@gmail.com>
2022-09-08 10:09:15 +0530

    data

---
+++ data-types/js/main.js
@@ -0,0 +1,62 @@
+// number data type
+var ab=20;
+console.log(ab);
+// string data type
+let d="ajay";
+console.log(d);
+
+let x=20 +"ajay";
+console.log(x);
+
+let y=10+5+"ajay"
+console.log(y);
+// array
+const names = ["ajay","sakthi","vicky"];
+console.log(names);
+console.log(typeof names)
+// object
+const person = {
+    fname : "Michael",
+    lname  : "Ajay",
+    age     : 24
+  };
+console.log(person);
+
+// boolean
+let a= 10;
+let b = 15;
+let z = 10;
+var aa = 20;
+var bb = '20'
+console.log(a==b);
+console.log(a===z);
+console.log(a==z);
+console.log(aa===bb);
+
+// typeof
+console.log(typeof 123);
+console.log(typeof "ajay");
+
+let m;
+console.log(m);
+
+let l="";
+console.log(l);
+
+var abc = 'ajay'+3+25;
+console.log(abc);
+
+// console.log(ajay);
+
+var cd = 5+'ajay'+6;
+console.log(cd);
+
+// var xy = null;
+// console.log(xy);
+var xyz = "";
+console.log(xyz)
+
+var bc = 5/"ajay";
+console.log(bc);
+
+
